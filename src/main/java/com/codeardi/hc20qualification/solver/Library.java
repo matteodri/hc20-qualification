13067ddb1434c98747017210a296e5d51a8a478c
Ardit Begaj <abegaj@xanadu.ie>
2020-02-20 19:44:24 +0100

    library

--- src/main/java/com/codeardi/hc20qualification/solver/Library.java
+++ src/main/java/com/codeardi/hc20qualification/solver/Library.java
@@ -1,5 +1,51 @@
 package com.codeardi.hc20qualification.solver;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Library {
 
+    private int id;
+    private int signUpDays;
+    private int booksPerDay;
+    private List<Book> books;
+    private Set<Book> bookPool;
+
+    public Library(int id, int signUpDays, int booksPerDay, List<Book> books, Set<Book> bookPool) {
+        this.id = id;
+        this.signUpDays = signUpDays;
+        this.booksPerDay = booksPerDay;
+        this.books = books;
+        this.books.sort(Comparator.naturalOrder());
+
+        this.bookPool = bookPool;
+    }
+
+    private int remainingSignUpDays = signUpDays;
+    private Set<Book> booksInScanning = new HashSet<>();
+    private Set<Book> scannedBooks = new HashSet<>();
+
+    public void dayElapsed() {
+        if (remainingSignUpDays > 0) {
+            remainingSignUpDays--;
+        } else {
+            scannedBooks.addAll(booksInScanning);
+            booksInScanning.clear();
+            for (int i = booksInScanning.size(); i < booksPerDay; i++) {
+                scanBook();
+            }
+
+        }
+    }
+
+    private void scanBook() {
+        for (Book book : books) {
+            if (bookPool.contains(book) && !booksInScanning.contains(book)) {
+                bookPool.remove(book);
+                booksInScanning.add(book);
+            }
+        }
+    }
 }
