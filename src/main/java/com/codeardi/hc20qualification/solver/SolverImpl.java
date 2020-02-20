0a6e649833db9cbceadaf7645c2e334edf83f328
Matteo Dri <mdri@xanadu.ie>
2020-02-20 19:49:08 +0100

    solve method

--- src/main/java/com/codeardi/hc20qualification/solver/SolverImpl.java
+++ src/main/java/com/codeardi/hc20qualification/solver/SolverImpl.java
@@ -13,6 +13,8 @@ public class SolverImpl implements Solver {
 
     protected static final Logger logger = LogManager.getLogger(SolverImpl.class);
 
+    private List<Book> listOfBooksAlreadySelected = new ArrayList<>();
+
     @Override
     public List<String> getSolution(List<String> inputLines) {
 
@@ -24,13 +26,35 @@ public class SolverImpl implements Solver {
 
 
     /**
+     * MAIN IMPLEMENTATION OF SOLUTION
      *
      * @return list of libraries picked. then each library will have the list of books selected
      */
     private List<Library> solve(int numberOfBooks, int numberOfLibraries, int numberOfDays, List<Book> books, List<Library> libraries){
         List<Library> result = new ArrayList<>();
 
+        List<Library> librariesToProcess = pickListOfLibraries(numberOfBooks, numberOfLibraries, numberOfDays, books, libraries);
 
+
+        return result;
+    }
+
+    /**
+     * This method can be just picking libraries in order of input or be smarter to optimise score picking libraries with most valueable books and less sign up time
+     * @return libraries in order to be processed
+     */
+    private List<Library> pickListOfLibraries(int numberOfBooks, int numberOfLibraries, int numberOfDays, List<Book> books, List<Library> libraries) {
+        List<Library> result = new ArrayList<>();
+
+        // pick by input order
+        result = new ArrayList<>(libraries);
+
+
+        // pick by score of books in each library
+
+
+
+        // pick maximising value (weight of books)/(sign up days)
 
 
         return result;
